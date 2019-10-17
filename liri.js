2169cfed6401dd331f169fe007ae55c3c2097e93
Andy Tran <aktran321@gmail.com>
2019-10-16 22:03:06 -0400

    Finished concert-this, movie-this, and spotify-this-song when arguments are logged in process.argv[3]. Still need to write code for when no inputs are given for process.argv[3]

--- liri.js
+++ liri.js
@@ -10,9 +10,10 @@ var request = require("request");
 var inputFunction = process.argv[2];
 
 var axios = require("axios");
-
-
+console.log("WE ARE STARTING HERE--------AGAIN-----------------------------")
+//The variable spotify is an object containing your client keys
 //console.log("This is spotify: "+ spotify);
+console.log("keys.spotify: "+JSON.stringify(keys.spotify));
 //console.log("This is Spotify: "+ Spotify);
 
 //---------------------------------------------------------
@@ -25,7 +26,7 @@ var axios = require("axios");
 var concertThis = function (){
     var artist = process.argv[3];
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(response){
-       console.log(response.data);
+       console.log(response);
        for(var i = 0; i<response.data.length; i++){
            console.log("-----------------------------");
            console.log(i+1+")");
@@ -36,9 +37,56 @@ var concertThis = function (){
     })
 };
 
+var spotifyThisSong = function() {
+    
+    var song=process.argv[3];
+    spotify.search({type:"track",query:song, limit:1}, function(error,response){
+        if (error){
+            return console.log("Error Occurred: "+error);
+        } else {
+            console.log("The response worked and is right here: " +JSON.stringify(response));
+            console.log("----------------------------");
+            console.log("Song Name: "+response.tracks.items[0].name);
+            console.log("Artist(s):");
+            for(var i = 0;i<response.tracks.items[0].artists.length; i++){
+                console.log(i+1+")"+response.tracks.items[0].artists[i].name);
+            }
+         console.log("Album Type: "+response.tracks.items[0].album.album_type);
+         console.log("Album Name: "+response.tracks.items[0].album.name);
+         console.log("Preview Link: "+ response.tracks.items[0].album.artists[0].external_urls.spotify);
+        }
+    })
+
+}
+
+var movieThis = function(){
+    var movie = process.argv[3];
+    axios.get("http://www.omdbapi.com/?t="+movie+"&y=&plot=short&apikey=trilogy").then(
+        function(response) {
+       console.log(response.data);
+       console.log("--------------------------");
+       console.log("Title: "+response.data.Title);
+       console.log("Release Year: "+ response.data.Year);
+       console.log("IMDB Rating: "+ response.data.imdbRating);
+       console.log("Rotten Tomato Rating: "+ response.data.Ratings[1].Value);
+       console.log("Country: "+response.data.Country);
+       console.log("Language: "+ response.data.Language);
+       console.log("Plot: "+ response.data.plot);
+       console.log("Actors: "+ response.data.Actors);
+   
+  }
+);
+}
+
 switch(inputFunction){
     case "concert-this":
         concertThis();
+        break;
+    case "spotify-this-song":
+        spotifyThisSong();
+         break;
+    case "movie-this":
+        movieThis();
         break;
 }
 
