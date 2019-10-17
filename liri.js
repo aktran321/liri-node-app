8fcac7b06f8b042164f1bf2c6605b7a14dee533e
Andy Tran <aktran321@gmail.com>
2019-10-17 12:16:40 -0400

    Broke up the concert, movie, and spotify searches into multiple functions to allow the do-what-it-says function to work properly. This also helps clean up the code much more

--- liri.js
+++ liri.js
@@ -25,24 +25,30 @@ console.log("WE ARE STARTING HERE--------AGAIN-----------------------------")
 // Date of the event and use moment to format it as MM/DD/YYYY
 var concertThis = function (){
     var artist = process.argv[3];
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(response){
-      // console.log(response);
-       for(var i = 0; i<response.data.length; i++){
-           console.log("-----------------------------");
-           console.log(i+1+")");
-           console.log("Name of Venue: "+ response.data[i].venue.name);
-           console.log("Venue Location: "+ response.data[i].venue.city+", "+response.data[i].venue.country);
-           console.log("Date: "+ response.data[i].datetime);
-       }
-    })
+    concertSearch(artist);
 };
-
+var concertSearch = function(artist){
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(response){
+        // console.log(response);
+         for(var i = 0; i<response.data.length; i++){
+             console.log("-----------------------------");
+             console.log(i+1+")");
+             console.log("Name of Venue: "+ response.data[i].venue.name);
+             console.log("Venue Location: "+ response.data[i].venue.city+", "+response.data[i].venue.country);
+             console.log("Date: "+ response.data[i].datetime);
+         }
+      })
+}
 var spotifyThisSong = function() {
     if(process.argv[3]){
         var song=process.argv[3];
     } else {
         var song = "The Sign"
     }
+    spotifySearch(song);
+}
+//                     Needed to define song in function(song) or else spotifySearch does not work
+var spotifySearch = function(song){
     spotify.search({type:"track",query:song, limit:1}, function(error,response){
         if (error){
             return console.log("Error Occurred: "+error);
@@ -59,12 +65,21 @@ var spotifyThisSong = function() {
          console.log("Preview Link: "+ response.tracks.items[0].album.artists[0].external_urls.spotify);
         }
     })
-
 }
+
+
 
 var movieThis = function(){
     if(process.argv[3]){
         var movie = process.argv[3];
+    }else{
+        var movie = "mr.nobody";
+    }
+    movieSearch(movie);
+    
+}
+
+var movieSearch = function(movie){
     axios.get("http://www.omdbapi.com/?t="+movie+"&y=&plot=short&apikey=trilogy").then(
         function(response) {
        console.log(response.data);
@@ -82,27 +97,26 @@ var movieThis = function(){
 ).catch(function(error){
     console.log("Error: +" + error);
 });
-    }else{
-        var movie = "mr.nobody";
-        axios.get("http://www.omdbapi.com/?t="+movie+"&y=&plot=short&apikey=trilogy").then(
-            function(response){
-        
-            console.log(response.data);
-            console.log("--------------------------");
-            console.log("Title: "+response.data.Title);
-            console.log("Release Year: "+ response.data.Year);
-            console.log("IMDB Rating: "+ response.data.imdbRating);
-            console.log("Rotten Tomato Rating: "+ response.data.Ratings[1].Value);
-            console.log("Country: "+response.data.Country);
-            console.log("Language: "+ response.data.Language);
-            console.log("Plot: "+ response.data.Plot);
-            console.log("Actors: "+ response.data.Actors);
-            }
-        ).catch(function(error){
-            console.log("Error: "+error);
-        })
-    };
-    
+
+}
+
+var doWhatItSays = function(){
+    fs.readFile("random.txt", "utf8",function(error, data){
+        if (error) {
+            return console.log(error);
+        }
+        //"Take the text and splits it into an array."
+        var commandArray = data.split(",");
+        console.log(commandArray);
+
+        if(commandArray[0]==="spotify-this-song"){
+            spotifySearch(commandArray[1]);
+        } else if(commandArray[0] === "movie-this"){
+            movieSearch(commandArray[1]);
+        } else if (commandArray[0] === "concert-this"){
+            concertSearch(commandArray[1]);
+        }
+    } )
 }
 
 switch(inputFunction){
@@ -115,4 +129,13 @@ switch(inputFunction){
     case "movie-this":
         movieThis();
         break;
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
 }
+
+
+
+// Still need to finish typing up code for the last function
+// and to take care of formatting the play times in concert-this.city
+// Everything else has been taken care of.
